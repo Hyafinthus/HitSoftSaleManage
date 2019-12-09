ac452da4faed508ed27bc16d9e63c265592ccb0b
e <hoyifu@gmail.com>
2019-12-09 23:09:21 +0800

    Init

---
+++ src/main/java/com/hit/soft/HitSoftSaleManageApplication.java
@@ -0,0 +1,13 @@
+package com.hit.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitSoftSaleManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitSoftSaleManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hit/soft/HitSoftSaleManageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hit.soft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HitSoftSaleManageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
