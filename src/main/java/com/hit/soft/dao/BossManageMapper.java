28ee3f82a6ebc1c5c390d97db76041cb504ee0dd
e <hoyifu@gmail.com>
2019-12-14 22:39:32 +0800

    pipixia-12/14/22:39

--- src/main/java/com/hit/soft/dao/BossManageMapper.java
+++ src/main/java/com/hit/soft/dao/BossManageMapper.java
@@ -3,18 +3,21 @@ package com.hit.soft.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.hit.soft.domain.Staff;
 
 @Mapper
 public interface BossManageMapper {
 	
-	public List<Staff> queryStaff();
+	public List<Staff> queryStaff(@Param("offset") Integer offset, @Param("limit")Integer limit);
 
 	public void addStaff(Staff staff);
 
 	public void deleteStaff(String staffId);
 
 	public void updateStaff(Staff staff);
+
+	public int countStaffs();
 	
 }
--- src/main/java/com/hit/soft/service/BossManageService.java
+++ src/main/java/com/hit/soft/service/BossManageService.java
@@ -6,12 +6,14 @@ import com.hit.soft.domain.Staff;
 
 public interface BossManageService {
 	
-	public List<Staff> queryStaff();
+	public List<Staff> queryStaff(int offset, int limit);
 
 	public void addStaff(Staff staff);
 
 	public void deleteStaff(String staffId);
 
 	public void updateStaff(Staff staff);
+
+	public int countStaffs();
 	
 }
