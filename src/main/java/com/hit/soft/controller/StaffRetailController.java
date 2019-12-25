29d2a0b59dbf342f7d662c255d34d17b3fe41182
e <hoyifu@gmail.com>
2019-12-25 23:46:32 +0800

    pipixia-12/25/23:46

--- src/main/java/com/hit/soft/controller/StaffRetailController.java
+++ src/main/java/com/hit/soft/controller/StaffRetailController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.hit.soft.domain.Client;
-import com.hit.soft.domain.JsonClient;
 import com.hit.soft.domain.JsonProduct;
 import com.hit.soft.domain.OrderProduct;
 import com.hit.soft.domain.Product;
@@ -28,31 +26,6 @@ public class StaffRetailController {
 	@Autowired
 	private StaffRetailService staffRetailService;
 	
-	//查询零售用户
-		@RequestMapping(value="/search/whosale_clients",method=RequestMethod.GET)
-		@ResponseBody
-		public String searchClients(HttpServletRequest request){
-			int count = staffRetailService.countClients();
-			int pageInt = Integer.parseInt(request.getParameter("page"));
-			int limitInt = Integer.parseInt(request.getParameter("limit"));
-			List<Client> data = staffRetailService.searchClients((pageInt - 1) * limitInt, limitInt);
-			JsonClient jsonClient = new JsonClient(count, data);
-			return JSONObject.fromObject(jsonClient).toString();
-		}
-		
-		@RequestMapping(value="/search/whosale_clients/{condition}",method=RequestMethod.GET)
-		@ResponseBody
-		public String searchClientsByCondition(@PathVariable String condition, HttpServletRequest request){
-			System.out.println(condition);
-			int count = staffRetailService.countClientsByCondition(condition);
-			System.err.println(count);
-			int pageInt = Integer.parseInt(request.getParameter("page"));
-			int limitInt = Integer.parseInt(request.getParameter("limit"));
-			List<Client> data = staffRetailService.searchClientsByCondition(condition, (pageInt - 1) * limitInt, limitInt);
-			JsonClient jsonClient = new JsonClient(count, data);
-			return JSONObject.fromObject(jsonClient).toString();
-		}
-		
 		//查询产品
 		@RequestMapping(value="/search/products",method=RequestMethod.GET)
 		@ResponseBody
